326eac6332f3c720d05fba19fbd8bfaaa36141dd
Michail96 <77673979+Michail96@users.noreply.github.com>
2025-06-05 21:39:49 +0200

    Add player and wheelbarrow models

---
+++ GrandBazaar/Models/Wheelbarrow.cs
@@ -0,0 +1,13 @@
+﻿namespace GrandBazaar.Models
+{
+    public class Wheelbarrow
+    {
+        public int MaxCapacityPerType { get; set; } = 3;
+
+        public int Jewelry { get; set; } = 0;
+        public int Fabric { get; set; } = 0;
+        public int Spice { get; set; } = 0;
+        public int Fruit { get; set; } = 0;
+        public int Rubies { get; set; } = 0;
+    }
+}
