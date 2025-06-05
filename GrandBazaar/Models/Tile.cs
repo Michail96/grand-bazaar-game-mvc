2ec29802d4a4c04f77d56a149ae2f9682e15904b
Michail96 <77673979+Michail96@users.noreply.github.com>
2025-06-05 21:24:01 +0200

    Basic project setup and Tile model

---
+++ GrandBazaar/Models/Tile.cs
@@ -0,0 +1,11 @@
+﻿namespace GrandBazaar.Models
+{
+    public class Tile
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Row { get; set; }
+        public int Col { get; set; }
+    }
+}
